63c77a9c14e6fd68666a80f774771810ba3bb8f9
henriquexu <38394541+henriquexu@users.noreply.github.com>
2019-04-29 13:35:56 +0800

    Get view

--- app/src/main/java/com/example/a300cem_assignment/Home.java
+++ app/src/main/java/com/example/a300cem_assignment/Home.java
@@ -2,8 +2,11 @@ package com.example.a300cem_assignment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
@@ -15,16 +18,34 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.a300cem_assignment.Common.Common;
+import com.example.a300cem_assignment.Model.Category;
+import com.example.a300cem_assignment.ViewHolder.MenuViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView txtUserName;
+
+    //Firebase
+    FirebaseDatabase database;
+    DatabaseReference categories;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter;
+
+    //View
+    RecyclerView recycler_menu;
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +55,20 @@ public class Home extends AppCompatActivity
         toolbar.setTitle("Menu");
         setSupportActionBar(toolbar);
 
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        categories = database.getReference("Categories");
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showDialog();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -55,6 +83,38 @@ public class Home extends AppCompatActivity
         View headerView = navigationView.getHeaderView(0);
         txtUserName = (TextView) headerView.findViewById(R.id.txtUserName);
         txtUserName.setText(Common.currentUser.getName());
+
+        //Init View
+        recycler_menu = (RecyclerView)findViewById(R.id.recycler_menu);
+        recycler_menu.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recycler_menu.setLayoutManager(layoutManager);
+        loadMenu();
+
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Home.this);
+        alertDialog.setTitle("Add new Category");
+        
+    }
+
+    private void loadMenu() {
+        adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(
+                Category.class,
+                R.layout.menu_item,
+                MenuViewHolder.class,
+                categories
+        ) {
+            @Override
+            protected void populateViewHolder(MenuViewHolder viewHolder, Category model, int position) {
+                viewHolder.txtMenuName.setText(model.getName());
+                Picasso.with(Home.this).load(model.getImage()).into(viewHolder.imageView);
+            }
+
+        };
+        adapter.notifyDataSetChanged();
+        recycler_menu.setAdapter(adapter);
 
 
     }
